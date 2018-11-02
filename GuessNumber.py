91b2d4e3e63240218309d1b68ce94cf94fea0bbd
kuba <jakuubsagan@gmail.com>
2018-11-02 17:12:09 +0100

    Adding comments to code

--- GuessNumber.py
+++ GuessNumber.py
@@ -1,16 +1,16 @@
 import random
 
-attempt_number = 0
+attempt_number = 0 # liczba prob
 
 print("Przedstaw się")
 name = input()
 
-computer_number = random.randint(1,20)
+computer_number = random.randint(1,20) # generuje losowa liczbe, ktora user bedzie zgadywal
 
 
 print("Witaj " + name + "wybrałem liczbę z zakresu 1 - 20")
 
-for attempt_number in range(6):
+for attempt_number in range(6): # 6 prob dopoki user nie zgadnie wartosci
     print("Spróbuj odgadnąć wartość")
     guess_attempt = input()
     
