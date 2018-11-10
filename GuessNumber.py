095b3e3466272428462d3b413364507526a66711
kuba <jakuubsagan@gmail.com>
2018-11-10 22:41:37 +0100

    Finishing code

--- GuessNumber.py
+++ GuessNumber.py
@@ -6,11 +6,26 @@ print("Przedstaw się")
 name = input()
 
 computer_number = random.randint(1,20) # generuje losowa liczbe, ktora user bedzie zgadywal
+computer_number = int(computer_number)
 
-
-print("Witaj " + name + "wybrałem liczbę z zakresu 1 - 20")
+print("Witaj " + name + ", komputer wylosował Ci liczbę z zakresu 1 - 20. Spróbuj ją odgadnąć")
 
 for attempt_number in range(6): # 6 prob dopoki user nie zgadnie wartosci
-    print("Spróbuj odgadnąć wartość")
-    guess_attempt = input()
-    
+    guess_attempt = int(input())
+
+    if guess_attempt > computer_number:
+        print("Twoja liczba jest za duża")
+
+    if guess_attempt < computer_number:
+        print("Twoja liczba jest za mała")
+
+    if guess_attempt == computer_number:
+        print("Trafiłeś")
+        break
+if guess_attempt == computer_number:
+    ("WIN!")
+    attempt_number = str(attempt_number + 1)
+    print("Udało Ci się po " + attempt_number + " próbach")
+else:
+    print("Przegrałeś")
+    print("Szukana przez Ciebie liczba to " + str(computer_number))
